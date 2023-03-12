327a35b902ad024409377ab87abc16748df8f2a2
Michael <michael.walton@g.austincc.edu>
2023-03-12 18:36:52 -0500

    removed list cpp and header file

---
+++ main.cpp
@@ -0,0 +1,14 @@
+/*
+ * File: main.cpp
+ * Author: Michael Walton
+ * Project: Number Sorting Game
+ */
+
+#include "main.h"
+
+int main() {
+
+    cout << "Hello, World!";
+
+    return 0;
+}
---
+++ main.h
@@ -0,0 +1,15 @@
+/*
+ * File: main.h
+ * Author: Michael Walton
+ * Project: Number Sorting Game
+ */
+
+#ifndef MAIN_H
+#define MAIN_H
+
+#include <iostream>
+
+using std::cin;
+using std::cout;
+
+#endif //MAIN_H
